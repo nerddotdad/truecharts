d1975b9ba1a4c7cda9de32bd2b36c3c25f6a781a
itzteajay-glitch <itzteajay@gmail.com>
2024-11-25 23:56:20 -0800

    testing the commenting out section

--- scripts/newapp.sh
+++ scripts/newapp.sh
@@ -5,7 +5,6 @@ chartSearch=True
 cd ~/truecharts/clusters/main/kubernetes/apps
 
 read -p "Enter new chart name: " chart
-echo "locating truecharts chart"
 read -p "Enter the namespace you'd like to put this chart in: " namespace
 
 while [ $chartSearch == True ]
@@ -35,11 +34,35 @@ sed -i -e "s/name: kubernetes-dashboard/name: ${chart}/g" helm-release.yaml
 sed -i -e "s/namespace: kubernetes-dashboard/namespace: ${namespace}/g" helm-release.yaml
 sed -i -e "s/chart: kubernetes-dashboard/chart: ${chart}/g" helm-release.yaml
 sed -i -e "s/version: 1.10.1/${latestVersion}/g" helm-release.yaml
-sed -i -e 's/loadBalancerIP: ${DASHBOARD_IP}/loadBalancerIP: YOUR IP HERE/g' helm-release.yaml
+
+while true; do
+
+read -p "Would you like to add a loadbalancer ip? (y/n) " yn
+
+case $yn in 
+	[yY] )
+    read -p "please enter the ip you'd like to expose to your INTERNAL network (usually 192,168.x.x)" ip
+    sed -i -e "s/loadBalancerIP: \${DASHBOARD_IP}/loadBalancerIP: ${ip}/g" helm-release.yaml
+	break
+    ;;
+	[nN] )
+    echo "Commenting out the values section. Please be sure to come back and edit it later!"
+    sed -i '/^  values:$/s/^/#/' helm-release.yaml
+    sed -i '/^    service:$/s/^/#/' helm-release.yaml
+    sed -i '/^      main:$/s/^/#/' helm-release.yaml
+    sed -i '/^        type: LoadBalancer$/s/^/#/' helm-release.yaml
+    sed -i '/^        loadBalancerIP: ${DASHBOARD_IP}$/s/^/#/' helm-release.yaml
+    sed -i '/^        ports:$/s/^/#/' helm-release.yaml
+    sed -i '/^          main:$/s/^/#/' helm-release.yaml
+    sed -i '/^            port: 80$/s/^/#/' helm-release.yaml
+    break
+    ;;
+	* ) echo invalid response;;
+esac
+
+done
 
 cat helm-release.yaml
-echo "please ensure you update the loadbalancer IP..."
-sleep 10
 read -p "does this look correct? (y/ctrl+c to quit)" helmVerify
 if [ $helmVerify != "y" ]
 then
